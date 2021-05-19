aca7b0e36d5012fea667cf6329e8e1f60c41f58d
johnpaul <johnpaulchukwu43@gmail.com>
2021-05-19 22:11:27 +0100

    Initial Commit

---
+++ src/main/java/com/jworks/qup/service/QupServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jworks.qup.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QupServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QupServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jworks/qup/service/QupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jworks.qup.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QupServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
